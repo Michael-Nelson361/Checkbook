f01e86c32b06cfb35f8b5cd2b298b95950868e74
Michael Nelson <zsc.michaeln@gmail.com>
2023-10-30 21:45:38 -0500

    Completed base requirements for project

--- checkbook.py
+++ checkbook.py
@@ -11,32 +11,67 @@ import subprocess
 
 # Open the balance file or create it if it doesn't exist
 if 'balances.txt' not in os.listdir():
-    f = open('balances.txt','w')
-    f.write('0')
-    f.close()
     balance = 0
 else:
     with open('balances.txt','r') as f:
-        balance = f.readline()
+        balance = float(f.readline())
 
 
-def menu1():
-    print('Option 1 not yet available.')
+def menu1(bal):
+    print('\t\t','*'*10,f'Your current balance is: ${bal:,.2f}')
 
-def menu2():
-    print('Option 2 not yet available.')
+def menu2(bal):
+    while True:
+        withdraw = input('Enter the amount you wish to withdraw: ')
+        
+        try:
+            return bal - float(withdraw)
+        except:
+            print('\t\t', withdraw,'is not a valid amount.')
 
-def menu3():
-    print('Option 3 not yet available.')
+def menu3(bal):
+    while True:
+        deposit = input('Enter the amount you wish to deposit: ')
+        
+        try:
+            return bal + float(deposit)
+        except:
+            print('\t\t', deposit, 'is not a valid amount.')
 
-def menu4():
-    print('Option 4 not yet available.')
+def menu4(bal):
+    with open('balances.txt','w') as f:
+        f.write(str(bal))
+
+
+while True:  
+    # Print the menu options
+    print("""
+          Menu:
+              1) View current balance
+              2) Submit withdrawal
+              3) Submit deposit
+              4) Quit
+          """)
+    menu_option = input('\rEnter a menu option: ')
     
-# Print the menu options
-print("""
-      Menu:
-          1) View current balance
-          2) Submit withdrawal
-          3) Submit deposit
-          4) Quit
-      """)
+    # Call the option defined
+    if menu_option == '1':
+        menu1(balance)
+    elif menu_option == '2':
+        balance = menu2(balance)
+    elif menu_option == '3':
+        balance = menu3(balance)
+    elif menu_option == '4':
+        menu4(balance)
+        print('Goodbye!')
+        break
+    # SECRET MENU OPTION
+    elif menu_option == '1812':
+        print(('='*50).center(100))
+        print('WARNING: KILLING BALANCE RECORD'.center(100))
+        print(f'ENDING BALANCE: ${balance:,.2f}'.center(100))
+        print(('='*50).center(100))
+        os.remove('balances.txt')
+        break
+    else:
+        print('Input invalid')
